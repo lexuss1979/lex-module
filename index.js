327d96e4be068f66be8c30bd60908966aabdc630
Lexuss <afanasyev.a.a@gmail.com>
2023-12-13 01:32:24 +0300

    version 1.2.0

--- index.js
+++ index.js
@@ -8,19 +8,65 @@ class stingObj  {
   duplicate() {
     return this.str + this.str;
   }
+  inverse() {
+    let inv = '';
+    for(let i=0; i< this.str.length; i++){
+        inv += this.str.slice(this.str.length-i-1,this.str.length-i);
+    }
+    return inv;
+  }
+
+  startsWith(needle){
+    return this.str.slice(0,needle.length) === needle;
+  }
+
+  endsWith(needle){
+    return this.str.slice(this.str.length-needle.length) === needle;
+  }
+
+  contains(needle){
+    return this.str.indexOf(needle) > -1;
+  }
+
+  truncate(length, start){
+    const startPos = start ?? 0;
+    return this.str.slice(startPos, startPos+length)
+  }
+
+  words(){
+    return this.str.replace(/\W+/g," ").replace(/\s+/g,' ').trim().split(' ').map(word => word.toLowerCase());
+    
+   }
+
+   wordsCount(){
+    return this.words().length;
+   }
+
+   wordsFreq(){
+    const words = [];
+    const freq = [];
+    
+    this.words().map(word => {
+      if(words.indexOf(word) > -1) {
+        freq[words.indexOf(word)] ++;
+      } else {
+        words.push(word);
+        freq.push(1);
+      }
+    })
+    return [words, freq];
+   }
+
+
+
+
 }
 
-const str = {
-
-
-
-  from: (str) => {
-    return new stingObj(str);
-  },
-  
+const strObj = (str) => {
+  return new stingObj(str);
 }
 
 
 
   
- module.exports = str;
+ module.exports = strObj;
--- test/script.js
+++ test/script.js
@@ -1,5 +1,17 @@
 // test/script.js
 
-const strMod = require('lex-module')
+// const strMod = require('lex-module')
+const str = require('../index')
 
-console.log(strMod.from('my team').duplicate())
+console.log(str('my team').inverse())
+console.log(str('my team').startsWith('me'))
+console.log(str('my team').startsWith('my'))
+console.log(str('my team').endsWith('team'))
+console.log(str('my team').endsWith('cofe'))
+console.log(str('my team').contains('cofe'))
+console.log(str('my team').contains('tea'))
+console.log(str('my team').truncate(3))
+console.log(str('my team').truncate(3,2))
+console.log(str('my team').words())
+console.log(str('my team').wordsCount())
+console.log(str('My team, my rule, my life. Life is great.').wordsFreq())
